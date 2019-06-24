2746a620ffa751586dd0b7cf3a33c36877b8e6c8
Aaron Whang and Holden Clark <aaron@>
2019-06-24 16:46:48 -0700

    add karma tester

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import $ from 'jquery';
 import './styles.css';
 import { pingPong } from './ping-pong';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
