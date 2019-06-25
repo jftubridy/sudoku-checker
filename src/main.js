661749d48514dee3b8cb30af4175cfa4b3d837c2
Holden Clark and Joseph Tubridy <holden@>
2019-06-25 14:30:08 -0700

    add function to allow user input and to check user input for valid solution

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import './styles.css';
-// import { sudokuCheck } from './sudoku-checker';
+import { SudokuCheck } from './sudoku-checker';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,13 +8,35 @@ $(document).ready(function() {
   $("#puzzle").submit(function(){
     event.preventDefault();
 
+    var board =[[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+    var currentRow = 0;
+
     for(var i = 1; i < 4; i++){
-      for(var j = 1; j <4; j++){
+      for(var j = 1; j < 4; j++){
         for(var k = 1; k < 10; k++){
-          var test = $(".bigrow-"+i+ " .row" +j+ " ." +k).val();
-          console.log(test);
+          if(isNaN(parseInt($(".bigrow-"+i+ " .row" +j+ " ." +k).val()))){
+            board[currentRow][k-1] = 0;
+          } else {
+            board[currentRow][k-1] = parseInt($(".bigrow-"+i+ " .row" +j+ " ." +k).val());
+          }
         }
+        currentRow++;
       }
     }
+    console.log(board);
+
+    var userSolution = new SudokuCheck(board);
+    var gameMessage = userSolution.checkBoard();
+    $("#result").html(gameMessage);
+
   });
 });
--- src/sudoku-checker.js
+++ src/sudoku-checker.js
@@ -2,3 +2,37 @@ export function SudokuCheck(spaces) {
   this.spaces = spaces;
 
 }
+
+SudokuCheck.prototype.checkBoard = function() {
+
+  var passRow = true, passCol = true;
+
+  for(var i = 0; i < this.spaces.length; i++){
+    for(var j = 0; j < this.spaces[0].length; j++){
+      for(var k = j + 1; k < this.spaces[0].length; k++){
+        if( this.spaces[i][j] === this.spaces[i][k] ){
+          passRow = false;
+        }
+
+      }
+    }
+  }
+
+  for(var y = 0; y < this.spaces.length; y++){
+    for(var u = 0; u < this.spaces[0].length; u++){
+      for(var h = u + 1; h < this.spaces[0].length; h++){
+        if( this.spaces[u][y] === this.spaces[h][y] ){
+          passCol = false;
+        }
+
+      }
+    }
+  }
+
+  if(passRow === true && passCol === true){
+    return "sweet victory";
+  } else {
+    return "try again";
+  }
+
+};
