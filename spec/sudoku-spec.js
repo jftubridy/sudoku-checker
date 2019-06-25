02af3dc0fa3d590c202ccfb212283ae36e6915ef
Holden Clark and Joseph Tubridy <holden@>
2019-06-25 11:53:16 -0700

    add input fields on sudoku board and add function to take user input

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -2,34 +2,52 @@ import { SudokuCheck } from './../src/sudoku-checker.js';
 
 describe('SudokuCheck', function() {
 
-  it('should test whether a row contains all numbers from 1 to 9 with no repeats', function() {
-    var row = new SudokuCheck([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  var board;
 
-    for(var i = 0; i < row.spaces.length; i++){
-      for(var j = i + 1; j < row.spaces.length; j++){
-        expect(row.spaces[i]).not.toEqual(row.spaces[j]);
-        console.log(row.spaces[i] + " compare " + row.spaces[j]);
-      }
-    }
-
-
+  beforeEach(function() {
+    // board = new SudokuCheck([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    //                                       [1, 2, 3, 4, 5, 6, 7, 8, 9]]);
+    
+    board = new SudokuCheck([[1, 5, 4, 8, 7, 3, 2, 9, 6],
+                                          [3, 8, 6, 5, 9, 2, 7, 1, 4],
+                                          [7, 2, 9, 6, 4, 1, 8, 3, 5],
+                                          [8, 6, 3, 7, 2, 5, 1, 4, 9],
+                                          [9, 7, 5, 3, 1, 4, 6, 2, 8],
+                                          [4, 1, 2, 9, 6, 8, 3, 5, 7],
+                                          [6, 3, 1, 4, 5, 7, 9, 8, 2],
+                                          [5, 9, 8, 2, 3, 6, 4, 7, 1],
+                                          [2, 4, 7, 1, 8, 9, 5, 6, 3]]);
   });
 
-  // it('should correctly determine whether three lengths can be made into a triangle', function() {
-  //   var notTriangle = new Triangle(3,9,22);
-  //   expect(notTriangle.checkType()).toEqual("not a triangle");
-  // });
+  it('should test whether any row contains all numbers from 1 to 9 with no repeats', function() {
+
+    for(var i = 0; i < board.spaces.length; i++){
+      for(var j = 0; j < board.spaces[0].length; j++){
+        for(var k = j + 1; k < board.spaces[0].length; k++){
+        expect(board.spaces[i][j]).not.toEqual(board.spaces[i][k]);
+        console.log(board.spaces[i][j] + " compare " + board.spaces[i][k]);
+        }
+      }
+    }
+  });
+
+  it('should test whether any column contains all numbers from 1 to 9 with no repeats', function() {
+
+    for(var i = 0; i < board.spaces.length; i++){
+      for(var j = 0; j < board.spaces[0].length; j++){
+        for(var k = j + 1; k < board.spaces[0].length; k++){
+        expect(board.spaces[j][i]).not.toEqual(board.spaces[k][i]);
+        console.log(board.spaces[j][i] + " compare " + board.spaces[k][i]);
+        }
+      }
+    }
+  });
 
 });
-
-// describe('Triangle', function() {
-//   var reusableTriangle;
-//
-//   beforeEach(function() {
-//     reusableTriangle = new Triangle(5, 5, 5);
-//   });
-//
-//   it('should show how beforeEach() works', function() {
-//     console.log(reusableTriangle);
-//   });
-// });
--- src/main.js
+++ src/main.js
@@ -5,5 +5,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
+  $("#puzzle").submit(function(){
+    event.preventDefault();
 
+    for(var i = 1; i < 4; i++){
+      for(var j = 1; j <4; j++){
+        for(var k = 1; k < 10; k++){
+          var test = $(".bigrow-"+i+ " .row" +j+ " ." +k).val();
+          console.log(test);
+        }
+      }
+    }
+  });
 });
